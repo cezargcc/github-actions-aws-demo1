f8001e352d78906f13542d51b8af04af0cb0b429
scissors <40953457+cezargcc@users.noreply.github.com>
2025-06-08 21:52:42 -0400

    Add files via upload

---
+++ application.py
@@ -0,0 +1,24 @@
+# Demo programm flask on AWS
+
+from flask import Flask, render_template
+
+application = Flask(__name__)
+
+@application.route("/")
+def root():
+    return render_template("index.html")
+    
+@application.route("/help")
+def helppage():
+    return render_template("help.html")
+    
+@application.route("/hello")
+def index():
+    return "Helo world from Flask Hello page.<b> v1.0"
+    
+#------------Main------------------
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
+    
+#----------------------------------

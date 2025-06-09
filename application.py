60c50ef4861fa14b7062a09dbb8e4aaf35b44d0b
scissors <40953457+cezargcc@users.noreply.github.com>
2025-06-08 23:12:17 -0400

    Update application.py

--- application.py
+++ application.py
@@ -4,21 +4,21 @@ from flask import Flask, render_template
 
 application = Flask(__name__)
 
+
 @application.route("/")
 def root():
     return render_template("index.html")
-    
+
 @application.route("/help")
 def helppage():
     return render_template("help.html")
-    
+
 @application.route("/hello")
 def index():
-    return "Helo world from Flask Hello page.<b> v1.0"
-    
-#------------Main------------------
+    return "Hello World from Flask Hello Page.<b> v1.0"
+
+#--------Main------------------
 if __name__ == "__main__":
     application.debug = True
     application.run()
-    
-#----------------------------------
+#------------------------------
